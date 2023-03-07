3ecd2b40eb452248f7afa2dfa553c3a216ffb1b4
Usmonbek Ravshanov <usmonbek_ravshanov@epam.com>
2023-03-07 12:32:43 +0500

    code_scanner section

--- slots/configuration.py
+++ slots/configuration.py
@@ -3,7 +3,7 @@ from pylon.core.tools import log, web
 
 class Slot:
     integration_name = 'security_scanner_spotbugs'
-    section_name = 'scanners'
+    section_name = 'code_scanners'
 
     @web.slot(f'integrations_{section_name}_content')
     def integration_create_modal_content(self, context, slot, payload):
